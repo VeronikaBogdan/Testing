ed75acb1468dd31f79a98fcf8d41f9ba43f9e254
VeronikaBogdan <bog-dan7777@mail.ru>
2022-11-20 23:04:58 +0300

    init: start work on lab10

---
+++ lab10/src/test/java/page/MarkFormelleProductPage.java
@@ -0,0 +1,47 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MarkFormelleProductPage extends AbstractPage{
+	private static final String PRODUCT_PAGE_URL = "https://markformelle.by/catalog/zhenshchinam/mf-life/bryuki-leginsy/182436-73175-1050/";
+	private final WebDriverWait webDriverWait = new WebDriverWait(webDriver, WAIT_SECONDS);
+
+	@FindBy(xpath = "//div[@class='size-header closed']");
+	private WebElement buttonOpenListOfSizes;
+
+	@FindBy(xpath = "//div[@data-offer-id='484144']");
+	private WebElement buttonSelectSize;
+
+	@FindBy(xpath = "//a[@href='javascript:void(0);']");
+	private WebElement buttonAddToCart;
+
+	@FindBy(xpath = "//span[@class='num' and text()='1']");
+	private WebElement amountOfGoods;
+
+	@FindBy(xpath = "//a[@data-addpickup='N' and @href='/personal/cart/']");
+	private WebElement buttonGoToCart;
+
+	public MarkFormelleProductPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public MarkFormelleProductPage clickButtonOpenListOfSizes() {
+		webDriverWait.until(ExpectedConditions.elementToBeClickable(buttonOpenListOfSizes));
+		return this;
+	}
+
+	public MarkFormelleProductPage clickButtonSelectSize() {
+		webDriverWait.until(ExpectedConditions.elementToBeClickable(buttonSelectSize));
+		return this;
+	}
+
+	@Override
+	public MarkFormelleProductPage openPage() {
+		webDriver.get(PRODUCT_PAGE_URL);
+		return this;
+	}
+}
